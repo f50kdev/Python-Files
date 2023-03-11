af6f23251615cb1e109492129cefedb2da1d83bc
Faustino Henriques <71952910+f50kdev@users.noreply.github.com>
2023-03-11 20:16:24 +0000

    fist exercicse in python

---
+++ exercise.py
@@ -0,0 +1,8 @@
+nome = input('Qual O seu nome');
+
+print('Seja Benvindo Senhor', nome);
+dia = input('Qual foi o seu dia de nascimento');
+mes = input('Insira o mes');
+ano = input('Insura o ano  que nasceste');
+
+print('voce neasceu no dia',dia , 'de' , mes , 'de ' , ano , 'Certo')
---
+++ scratch.py
@@ -0,0 +1,7 @@
+name = input('insira o seu nome')
+idade = input('insira a sua idade')
+
+peso = input(' insra o seu = peso')
+
+print(  name, idade , peso)
+print('ola  Mundo')
